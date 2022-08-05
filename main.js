279d6cd16eae2bb09440a693050b25a0f8f070a6
Zuurakan Saparbaieva <zuurakans@gmail.com>
2022-08-05 14:09:26 +0300

    added json file

--- main.js
+++ main.js
@@ -1,85 +1,121 @@
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'vertical',
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
-// });
-
 
 /*Getting data */ 
 
 async function getResponce (){
   let responce=await fetch("./db.json");
   let content=await responce.json();
-  let pets__wrap=document.querySelector('.pets__wrap');
-  for (let key in content){
-    pets__wrap.innerHTML+= `
+  let petsWrap=document.querySelector('.pets__wrap');
+  for (let item in content){
+    petsWrap.innerHTML+= `
     <div class="friends__item pets__item">
-    <img class="friends__image" src="${content[key].img}" alt="dog">
-            <p class="friends__text">${content[key].name}</p>
+    <img class="friends__image" src="${content[item].img}" alt="dog">
+            <p class="friends__text">${content[item].name}</p>
             <button class="friend__button">Learn more</button>
           </div>
         </div>
     `
-
   }
 }
 getResponce()
 
+//---------------------Getting data-----------------------------------------------
+fetch ('./db.json')
+.then((responce)=>{
+  return responce.json();
+})
+.then(data=>{
+    
+    data.map((value)=>{
+      document.querySelector('.carousel').innerHTML+=`
+      
+       <div class="friends__item friend-dog">
+       <img class="friends__image" src="${value.img}" alt="dog">
+       <p class="friends__text">${value.name}</p>
+       <button class="friend__button">Learn more</button>
+ </div>
+ `
+    })  
+})
 
-/* Popup */
-const background=document.querySelector('.popup__back');
-const popup=document.querySelector('.popup');
-const popup2=document.getElementById('.popup2');
-const popup3=document.getElementById('.popup3');
-const openPopup=document.querySelectorAll('.friends__item');
-const popupWrapper=document.querySelector('popup__wrapper')
+//-----------Carousel--------------------------------------------------------------
+
+const btnLeft=document.querySelector('#btn__left');
+const btnRight=document.querySelector('#btn__right');
+const carousel=document.querySelector('#carousel');
+
+const moveRight=function(){
+  carousel.classList.add("transition-right");
+  btnRight.removeEventListener('click',moveRight)
+}
+
+const moveLeft=()=>{
+carousel.classList.add ("transition-left");
+btnLeft.removeEventListener('click',moveLeft)
+}
+
+btnRight.addEventListener('click',moveRight );
+btnLeft.addEventListener('click',moveLeft)
+
+
+
+carousel.addEventListener('animationend',()=>{
+  carousel.classList.remove("transition-left");
+  carousel.classList.remove("transition-right");
+  btnRight.addEventListener('click',moveRight );
+  btnLeft.addEventListener('click',moveLeft)
+})
+
+
+
+
+
+
+
+/*---------------------Popup----------------------------------------------------*/
+
+
+// const popupWrapper=document.querySelector('popup__wrapper')
 
 // openPopup.forEach((friend)=>{
 //   friend.addEventListener('click',handleClick)
 // });
 
 // function handleClick(e){
-//   // background.classList.add('active');
-//   // popup.classList.add('active');
+  // background.classList.add('active');
+  // popup.classList.add('active');
 
 // }
 
-openPopup[0].addEventListener('click',function (){
-  background.style.display="block";
-  popup.style.display="block";
-})
-
-openPopup[1].addEventListener('click',function (){
-  background.style.display="block";
-  popup2.style.display="block";
-})
-openPopup[2].addEventListener('click',function (){
-  background.style.display="block";
-  popup3.style.display="block";
-})
-
-document.addEventListener('click', (e) => {
+// document.addEventListener('click', (e) => {
    
-  if(e.target === popup) { 
-     background.classList.remove('active');
-      popup.classList.remove('active');
-      openPopup.classList.remove('active'); 
+//   if(e.target === popup) { 
+//      background.classList.remove('active');
+//       popup.classList.remove('active');
+//       openPopup.classList.remove('active'); 
       
-  }
-});
+//   }
+// });
+const background=document.querySelector('.popup__back');
+const popup=document.querySelector('.popup');
+const openPopup=document.querySelectorAll('.friends__item');
+//-----------------------------------------------------------------------//
+const btns=document.querySelectorAll("[data-target]");
+const closeBtns=document.querySelectorAll(".modal-btn");
+const overlay=document.querySelector(".overlay");
+
+btns.forEach((btn)=>{
+  btn.addEventListener('click',()=>{
+    document.querySelector(btn.dataset.target).classList.add("active");
+    overlay.classList.add("active");
+    
+  })
+ 
+})
+
+closeBtns.forEach((btn)=>{
+  btn.addEventListener('click',()=>{
+  document.querySelector(btn.dataset.target).classList.remove("active");
+  overlay.classList.remove("active");
+  })
+  
+})
